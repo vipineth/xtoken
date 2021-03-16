9c23f559dcd36a473c18e0ea01b8d7de19cff71d
Ankit <iankit17@gmail.com>
2021-03-16 13:47:03 +0530

    update eth provider

--- components/loading.js
+++ components/loading.js
@@ -1,7 +1,12 @@
 function Loading() {
   return (
     <div className='w-full h-full fixed top-0 left-0 bg-pu z-50 bg-color flex justify-center items-center'>
-      <div className='donut '></div>
+      <div className='flex flex-col justify-center items-center'>
+        <div className='donut '></div>
+        <p className='text-lg font-bold pt-6 text-white'>
+          Loading required data from blockchain!
+        </p>
+      </div>
     </div>
   );
 }
